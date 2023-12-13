4e9abbf4c9aa84f43abb75f14675a5dffb71f731
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2023-12-13 22:49:10 +0300

    update

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,21 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self,name,breed):
+        self._name=name
+        self._breed=breed
+
+    def get_name(self):
+        print ("Retrieving name.")
+        return self._name
+    
+    def set_name(self,name):
+        if (not isinstance(name,str)) or not (1<= len(name) <=25):
+           print ("Name must be string between 1 and 25 characters")
+            
+        
+        else:
+            self._name=name   
+
+fido= Dog(123)
+print (fido._name)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,36 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name, job):
+        self._name = None  
+        self._job = None   
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (0 < len(value) < 25):
+            print("Name must be a string under 25 characters.")
+        else:
+            self._name = value.title()
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        approved_jobs =APPROVED_JOBS()
+        if value not in approved_jobs:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = value
+
+
+person = Person("Guido", "Sales")
+print(person.name)  
+print(person.job)   
+
+
